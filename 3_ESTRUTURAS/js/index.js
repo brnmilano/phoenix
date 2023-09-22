362e1661d0c7a5e54de343ba181d1114063022cf
Bruno Milano <brn.milano@hotmail.com>
2023-09-21 21:22:37 -0300

    Structures

---
+++ 3_ESTRUTURAS/js/index.js
@@ -0,0 +1,47 @@
+// 1 - Variáveis
+let nome = "Gabriel";
+
+console.log(nome);
+
+nome = "Gabriel Santos";
+
+console.log(nome);
+
+const idade = 21;
+
+console.log(idade);
+
+//idade = 22;
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+// 2 - Funções do JS: Prompt
+//const getPrompt = prompt("Digite sua idade:")
+
+//console.log(`Você tem ${getPrompt} anos.`);
+
+// 3 - Funções do JS: Math.x
+console.log(Math.max(5, 2, 3, 10));
+
+console.log(Math.floor(5.15));
+
+console.log(Math.ceil(5.15));
+
+// 4 - Funções do JS: Console.x
+console.log("teste");
+console.error("erro")
+console.warn("aviso");
+
+// 7 - IF
+let número = 10
+
+if (número > 5) {
+  console.log("Número é maior que 5");
+}
+
+const user = "Bruno";
+
+if (user === "Bruno") {
+  console.log("Olá Bruno");
+}
