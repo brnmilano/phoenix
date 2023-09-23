08aa513ca77a651dbfc3bdeadc35e85852d47021
Bruno Milano <brn.milano@hotmail.com>
2023-09-22 21:07:40 -0300

    Structures

--- 3_ESTRUTURAS/js/index.js
+++ 3_ESTRUTURAS/js/index.js
@@ -45,3 +45,98 @@ const user = "Bruno";
 if (user === "Bruno") {
   console.log("Olá Bruno");
 }
+
+// 8 - Else
+
+const loggedIn = false;
+
+if (loggedIn) {
+  console.log("Está autenticado");
+} else {
+  console.log("Não está autenticado");
+}
+
+const z = 10;
+const x = 15;
+
+if (z > 5 && z > 20) {
+  console.log("Números mais altos");
+} else {
+  console.log("Os números não são mais altos");
+}
+
+// 9 - Else If
+if (0 > 2) {
+  console.log("Agora não");
+} else if (1 > 2) {
+  console.log("Agora não");
+} else if (2 > 3) {
+  console.log("Agora não");
+} else if (3 > 2) {
+  console.log("Agora sim");
+}
+
+// 10 - While
+
+let p = 0;
+
+while (p < 5) {
+  console.log(`Repetindo ${p}`);
+  p = p + 1;
+  //p++;
+};
+
+// 11 - Do While
+let o = 10;
+
+do {
+  console.log(`Valor de o: ${o}`);
+  o--;
+} while (o > 1);
+
+console.log("-");
+
+// 12 - For
+
+for (let i = 0; i < 10; i++) {
+  console.log(`Repetindo algo`);
+}
+
+let r = 10;
+
+for (r; r > 10; r = r - 1) {
+  console.log(`O r está diminuindo`);
+}
+
+// 13 - Break
+
+for (let u = 20; u > 10; u--) {
+  console.log(`O valor de ${u}`);
+
+  if (u === 12) {
+    console.log(`o g é 12`);
+    break;
+  }
+}
+
+// 14 - Switch
+
+const job = "Advogado"
+
+switch (job) {
+  case "Programador":
+    console.log("Você é um Programador");
+    break;
+
+  case "Advogado":
+    console.log("Você é um Advogado");
+    break;
+
+  case "Engenheiro":
+    console.log("Você é um Engenheiro");
+    break;
+
+  default:
+    console.log("Profissão não encontrada");
+    break;
+}
