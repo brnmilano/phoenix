02ebc28045981f1de3c5098cf4748d6e8a2c122e
Bruno Milano <brn.milano@hotmail.com>
2023-09-24 20:57:30 -0300

    Object Orientation with JavaScript

--- 6_OOP/js/index.js
+++ 6_OOP/js/index.js
@@ -175,3 +175,99 @@ Caminhao.prototype.motor = 4.0
 const c3 = new Caminhao(6, "Rosa")
 
 console.log(c3.motor);
+
+// 11 - Override
+
+class Humano {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+}
+
+const bruno = new Humano("Bruno", 28);
+
+console.log(bruno);
+
+Humano.prototype.idade = "Não defindo";
+
+console.log(bruno.idade);
+console.log(Humano.prototype.idade);
+
+// 12 - Symbols
+
+class Aviao {
+  constructor(marca, turbinas) {
+    this.marca = marca;
+    this.turbinas = turbinas;
+  }
+}
+
+const asas = Symbol();
+const pilotos = Symbol();
+
+Aviao.prototype[asas] = 2;
+Aviao.prototype[pilotos] = 3;
+
+const boeing = new Aviao("Boeing", 10);
+
+console.log(boeing);
+console.log(boeing[asas]);
+console.log(boeing[pilotos]);
+
+// 13 - Getters e Setters 
+
+class Post {
+  constructor(titulo, descricao, tags) {
+    this.titulo = titulo;
+    this.descricao = descricao;
+    this.tags = tags;
+  };
+
+  get exibirTitulo() {
+    return `Você está lendo: ${this.titulo}`
+  };
+
+  set adicionarTags(tags) {
+    const tagsArray = tags.split(", ");
+    this.tags = tagsArray;
+  };
+};
+
+const myPost = new Post("Algum post", "É um post sobre programação");
+
+console.log(myPost);
+console.log(myPost.exibirTitulo);
+
+myPost.adicionarTags = "Programação, JS";
+
+console.log(myPost);
+
+// 14 - Herança
+
+class Mamifero {
+  constructor(patas) {
+    this.patas = patas;
+  };
+};
+
+class Lobo extends Mamifero {
+  constructor(patas, nome) {
+    super(patas, nome);
+
+    this.nome = nome;
+  }
+}
+
+const shark = new Lobo(4, "Shark")
+
+console.log(shark);
+console.log(shark.patas);
+
+// 15 - Operador Instanceof
+
+console.log(shark instanceof Lobo);
+
+console.log(Lobo instanceof Mamifero);
+
+console.log(new Lobo(4, "teste") instanceof Mamifero);
